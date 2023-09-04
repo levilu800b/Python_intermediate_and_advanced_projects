af3b497f1956d72d132caa72e5598875dd860797
levilu800b <levilu800b@gmail.com>
2023-09-04 20:09:06 +0100

    Coffee machine OOP complete

--- Coffee Machine OOP/main.py
+++ Coffee Machine OOP/main.py
@@ -1,3 +1,25 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# TODO 1. Print report.
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+# TODO 2. Check resources sufficient?
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and \
+                money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
