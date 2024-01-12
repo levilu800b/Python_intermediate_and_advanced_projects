039e361f0502ffd597572a8a160a96c4ef234952
levilu800b <levilu800b@gmail.com>
2024-01-12 12:45:46 +0000

    Frash card creating a UI

--- Flash card app - Capstone /main.py
+++ Flash card app - Capstone /main.py
@@ -1,2 +1,27 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# Canvas
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="./images/card_front.png")
+card_back_img = PhotoImage(file="./images/card_back.png")
+card_front = canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(column=0, row=0, columnspan=2)
+
+# Buttons
+cross_image = PhotoImage(file="./images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button.grid(column=0, row=1)
+
+check_image = PhotoImage(file="./images/right.png")
+known_button = Button(image=check_image, highlightthickness=0)
+known_button.grid(column=1, row=1)
+
+window.mainloop()
 
