5c60573c4eea655a2265599c1e4cfd9d62de6acb
levilu800b <levilu800b@gmail.com>
2024-01-15 20:15:08 +0000

    bugs

--- Rain alert/main.py
+++ Rain alert/main.py
@@ -6,13 +6,14 @@ OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = os.environ.get("MY_API")
 account_sid = os.environ.get("MY_SID")
 auth_token = os.environ.get("MY_AUTH")
+my_number = os.environ.get("MY_NUMBER")
+twilio_number = os.environ.get("TWILIO_NUMBER")
 
 weather_params = {
-    "lat": 53.383331,
-    "lon": -1.466667,
+    "lat": -10.686260,
+    "lon": -77.774567,
     "appid": api_key,
-    "units": "metric",
-    "cnt": 12,
+    "cnt": 4,
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
@@ -25,4 +26,11 @@ for hour_data in weather_data["list"]:
     if int(condition_code) < 700:
         will_rain = True
 if will_rain:
-    print("Bring an umbrella.")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☂️",
+        from_=twilio_number,
+        to=my_number
+    )
+    print(message.status)
