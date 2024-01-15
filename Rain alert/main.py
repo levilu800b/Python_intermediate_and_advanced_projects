edb30b1e92aa59e58b1054a1b2c9f34108110ec7
levilu800b <levilu800b@gmail.com>
2024-01-15 18:56:25 +0000

    Check if it will rain in the next 12 hours

--- Rain alert/main.py
+++ Rain alert/main.py
@@ -12,4 +12,13 @@ weather_params = {
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella.")
