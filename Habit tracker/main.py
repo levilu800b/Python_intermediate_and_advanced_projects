0d782f2ceb67d7daec1364f6aa120fd3bb152854
levilu800b <levilu800b@gmail.com>
2024-01-18 14:58:01 +0000

    Habit tracker added the env for my keys

--- Habit tracker/main.py
+++ Habit tracker/main.py
@@ -1,13 +1,40 @@
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Create a user
+
+USERNAME = os.getenv("USERNAME")
+TOKEN = os.getenv("TOKEN")
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "",
-    "username": "",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+# Create a graph
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Work Graph",
+    "unit": "hours",
+    "type": "float",
+    "color": "ajisai",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
