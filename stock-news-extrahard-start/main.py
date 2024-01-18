911c06bb1a8f9d316747b113c82717f9245a8e6d
levilu800b <levilu800b@gmail.com>
2024-01-18 13:56:16 +0000

    articles

--- stock-news-extrahard-start/main.py
+++ stock-news-extrahard-start/main.py
@@ -7,6 +7,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = ""
+NEWS_API_KEY = ""
 
 stock_parameters = {
     "function": "TIME_SERIES_DAILY",
@@ -26,3 +27,14 @@ day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
 
 diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+
+if diff_percent > 0:
+    news_parameters = {
+        "qInTitle": COMPANY_NAME,
+        "apiKey": NEWS_API_KEY,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_parameters)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+    print(three_articles)
