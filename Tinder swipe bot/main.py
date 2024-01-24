ff8376568ca0fb0e4a5d2e5c2a2f155bb724e308
levilu800b <levilu800b@gmail.com>
2024-01-24 14:45:56 +0000

    tinder swipe bot errors

--- Tinder swipe bot/main.py
+++ Tinder swipe bot/main.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 from time import sleep
 
-FB_EMAIL = YOUR FACEBOOK LOGIN EMAIL
-FB_PASSWORD = YOUR FACEBOOK PASSWORD
+FB_EMAIL = "YOUR FACEBOOK LOGIN EMAIL"
+FB_PASSWORD = "YOUR FACEBOOK PASSWORD"
 
 chrome_driver_path = "/Users/lenargasimov/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
