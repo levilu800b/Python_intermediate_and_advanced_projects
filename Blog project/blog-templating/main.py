ba4675d5b5bb239be374295559c9f19287eaefd4
levilu800b <levilu800b@gmail.com>
2024-02-01 16:13:35 +0000

    blog test2

--- Blog project/blog-templating/main.py
+++ Blog project/blog-templating/main.py
@@ -2,30 +2,11 @@ from flask import Flask, render_template
 from post import Post
 import requests
 
-# Fetching posts from the API
-response = requests.get("https://api.npoint.io/5abcca6f4e39b4955965")
-
-# Checking if the request was successful (status code 200)
-if response.status_code == 200:
-    posts = response.json()
-else:
-    # If the request was not successful, print an error message and exit
-    print(f"Error fetching posts. Status code: {response.status_code}")
-    exit()
-
+posts = requests.get("https://api.npoint.io/5abcca6f4e39b4955965").json()
 post_objects = []
-
-# Checking if the received data is a list
-if isinstance(posts, list):
-    for post in posts:
-        # Checking if the necessary keys exist in each post
-        if all(key in post for key in ["id", "title", "subtitle", "body"]):
-            post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-            post_objects.append(post_obj)
-        else:
-            print("Invalid post format. Skipping.")
-else:
-    print("Invalid data format. Expected a list of posts.")
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
@@ -37,13 +18,14 @@ def get_all_posts():
 
 @app.route("/post/<int:index>")
 def show_post(index):
-    requested_post = next((blog_post for blog_post in post_objects if blog_post.id == index), None)
-
-    if requested_post:
-        return render_template("post.html", post=requested_post)
-    else:
-        return "Post not found", 404
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
